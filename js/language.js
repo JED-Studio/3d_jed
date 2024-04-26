50166bae1a8ca17570bc8281d8507df105a8342a
Ramazan <romagt2015@mail.ru>
2024-04-26 16:07:01 +0300

    Home_page

--- js/language.js
+++ js/language.js
@@ -45,48 +45,52 @@ document.addEventListener("DOMContentLoaded", function() {
         en: "A project for Xiaomi for its new Poco M3 smartphone"
       },
       "home_page-10": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "3D-движение для Xiaomi и ее смартфона Redmi Note",
+        en: "3D Motion for Xiaomi and its Redmi Note smartphone"
       },
       "home_page-11": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "Марка игровых наушников Dexp Storm Pro 7.1",
+        en: "Brand of gaming headphones Dexp Storm Pro 7.1"
       },
       "home_page-12": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "ЭТАПЫ РАБОТЫ",
+        en: "STAGES OF WORK"
       },
       "home_page-13": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "КОНЦЕПТ",
+        en: "THE CONCEPT"
       },
       "home_page-14": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "Мы выявим все особенности продукта, целевую аудиторию и задачи ролика для написания трех разных версий сценария",
+        en: "We will identify all the features of the product, the target audience and the objectives of the video for writing three different versions of the script"
       },
       "home_page-15": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "АНИМАЦИЯ",
+        en: "ANIMATION"
       },
       "home_page-16": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "На основе раскадровки, показанной на этапе концепта, мы создадим 3D ролик со всеми анимациями",
+        en: "Based on the storyboard shown at the concept stage, we will create a 3D video with all the animations"
       },
       "home_page-17": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "МАТЕРИАЛЫ",
+        en: "MATERIALS"
       },
       "home_page-18": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "Этап, на котором вы увидете готовый ролик с материалами и светом в финальном качестве",
+        en: "The stage where you will see the finished video with materials and light in the final quality"
       },
       "home_page-19": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "ФИНАЛ",
+        en: "FINAL"
       },
       "home_page-20": {
-        ru: "Первый параграф",
-        en: "First paragraph"
+        ru: "Красиво уложу все по папкам, вышлю ссылку на облако Подарю чек лист со способами использования проекта",
+        en: "I'll put everything in folders beautifully, send a link to the cloud, and give you a checklist with ways to use the project"
+      },
+      "home_page-21": {
+        ru: "ВСЕ РАБОТЫ",
+        en: "ALL WORKS"
       },
 
 
